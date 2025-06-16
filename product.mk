b974ed59e231a152d6f940888cbe7ae21b4b4f6a
AdarshGrewal <adarshgrewal@gmail.com>
2025-06-17 02:22:44 +0530

    extra: Include Pixel 1 feature sysconfig
    Change-Id: I26c1c420fa40904343d5d9ffe741dcf41452b249

--- product.mk
+++ product.mk
@@ -22,6 +22,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# Gphotos
+PRODUCT_COPY_FILES += \
+    vendor/extra/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # MiuiCamera
 $(call inherit-product-if-exists, device/xiaomi/$(shell echo -n $(TARGET_PRODUCT) | sed -e 's/^[a-z]*_//g')-miuicamera/device.mk)
 
