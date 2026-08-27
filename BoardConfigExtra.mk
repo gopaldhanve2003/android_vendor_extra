2ca39170daab5e1d201b5dcfc36d06887380a448
gopaldhanve2003 <143928521+gopaldhanve2003@users.noreply.github.com>
2026-08-27 17:41:06 +0530

    extra: Some cleanup

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,9 +1,6 @@
 # Pixel
 include vendor/pixel/clocks/products/board.mk
 
-# MiuiCamera
--include device/xiaomi/$(PRODUCT_DEVICE)-miuicamera/BoardConfig.mk
-
 # Sepolicy
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     vendor/extra/sepolicy/private
