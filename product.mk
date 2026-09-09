7ca19f9afefeb5b993a232d8f78cb4e0fed3231e
AdarshGrewal <adarshgrewal@gmail.com>
2026-09-09 09:26:23 +0530

    extra: Drop config_certifiedBuildProperties overlay
    Change-Id: I743fa29e9cf9e83ba17b2654dc6d771844ed8efb

--- product.mk
+++ product.mk
@@ -28,8 +28,7 @@ endif
 
 PRODUCT_PACKAGES += \
     ExtraSettingsResTarget \
-    ExtraUpdaterOverlay \
-    ExtraPIFrameworksResTarget
+    ExtraUpdaterOverlay
 
 # Sysconfig
 PRODUCT_COPY_FILES += \
